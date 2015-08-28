f602bc0b1acc01092791b808ee44562915390f8f
Sunil Unka <sunil.unka@gmail.com>
2015-08-28 15:46:20 +1200

    fsgg.js

--- fsgg.js
+++ fsgg.js
@@ -29,22 +29,31 @@
     } else if(this.isRepeat(this.playerGuesses, guess)){
       console.error("You have already used that sonobuoy, try again!");
     } else {
-      this.tempOfGuess(guess);
+      this.guessTemp(guess);
       this.playerGuesses.push(guess);
       console.log(this.playerGuesses);
     }
   }
 
-  Game.prototype.tempOfGuess = function(guess){
-    if(this.numberToGuess -  guess > 0){ 
-      if((this.numberToGuess - guess <= 2)){
-        console.info("Scorching hot!");
-      }
-    } else if (this.numberToGuess - guess < 0){
-      if(this.numberToGuess - guess >= 2){
-        console.info("Scorching hot!");
-      }
-    }  
+  Game.prototype.guessTemp = function(guess){
+    var guessDifference = this.numberToGuess - guess;
+    var diff = guessDifference > 0 ? guessDifference : (guessDifference * -1);
+    if(diff <= 2){
+      console.log("Scorching hot!");
+    } else if ((diff > 2) && (diff <= 5)){
+      console.info("Really hot!");
+    } else if ((diff > 5) && (diff <= 10)) {
+      console.info("Hot!")
+    } else if ((diff > 10) && (diff <= 20)) {
+      console.log("Warm");
+    } else if ((diff > 20) && (diff <= 35)){
+      console.info("Cold");
+    } else if ((diff > 35) && (diff <= 50)){
+      console.info("Really cold");
+    } else {
+      console.info("Freezing cold!");
+    }
+
   }
 
   Game.prototype.compareLastGuess = function(guess, arr){
